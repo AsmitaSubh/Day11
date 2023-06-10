c0b7de9dce0a7c230dba4064b190f2e8fef880f4
AsmitaSubh <saswain92@gmail.com>
2023-06-10 18:39:34 +0530

    fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,14 @@
+public class Fibonacci
+{
+    public static void main(String[] args) {
+        int num = 10,firstnum = 0,secondnum = 1;
+        System.out.println("fibonacci starting 10 num");
+        for(int i =1;i<num;i++){
+            System.out.print(firstnum +" " );
+            int nextnum = firstnum + secondnum;
+            firstnum = secondnum;
+            secondnum = nextnum;
+
+        }
+    }
+}
